8f7c5c9061dd2fe9c96a4d430752542c6883b169
abhishek <akpathak22@iitk.ac.in>
2026-01-23 00:23:17 +0530

    Updates

--- assets/IndiaMap-rqaEM8nX.js
+++ assets/IndiaMap-rqaEM8nX.js
@@ -1,4 +1,4 @@
-import{c as h2,r as Z,R as he,a as u2,g as l2,b as c2,j as E,M as f2,C as d2}from"./index-C5Lf81yG.js";/**
+import{c as h2,r as Z,R as he,a as u2,g as l2,b as c2,j as E,M as f2,C as d2}from"./index-_C7qwYzV.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
